a37b461cd9e2772c75a328dfc0edbc33cee8ba83
Defou1ts <pasnkevikn@gmail.com>
2023-04-19 14:28:05 +0300

    feat: added fonts processing

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import '@shared/theme/global.scss';
 import '@shared/theme/normalize.scss';
+import { inter, sen } from '@shared/lib/fonts';
+
+import cn from 'classnames';
 
 import type { AppProps } from 'next/app';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return <Component className={cn(inter.className, sen.className)} {...pageProps} />;
 }
