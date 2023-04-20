84a9211a5b6e3981939f313c0a88d5cb5db08a7d
Defou1ts <pasnkevikn@gmail.com>
2023-04-20 12:19:58 +0300

    feat: button component

---
+++ src/shared/ui/Button/index.stories.tsx
@@ -0,0 +1,24 @@
+import { Button } from '.';
+
+import type { Meta, StoryFn } from '@storybook/react';
+
+const ButtonMeta: Meta<typeof Button> = {
+	title: 'Button',
+	component: Button,
+};
+
+export default ButtonMeta;
+
+const Template: StoryFn<typeof Button> = (args) => <Button {...args}>Hello</Button>;
+
+export const Primary = Template.bind({});
+
+Primary.args = {
+	type: 'primary',
+};
+
+export const Secondary = Template.bind({});
+
+Primary.args = {
+	type: 'secondary',
+};
