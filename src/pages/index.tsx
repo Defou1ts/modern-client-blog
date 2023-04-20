7b00bfc55200e340f886287ba8aae589bb6c5604
Defou1ts <pasnkevikn@gmail.com>
2023-04-20 13:46:33 +0300

    fix: styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import { MainContainer } from '@app/MainContainer';
+import { H } from '@shared/ui/H';
 
 const Index = () => (
 	<MainContainer title="Home | Modsen client blog" description="Modsen client blog">
 		<div>Hello</div>
+		<H type="display">Hello</H>
 	</MainContainer>
 );
 
