1b0660839081821cb91a43b1b59a4ae5ea15231b
Defou1ts <pasnkevikn@gmail.com>
2023-04-20 20:50:10 +0300

    feat: header component mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@shared/theme/global.scss';
 import '@shared/theme/normalize.scss';
+import '@shared/theme/global.scss';
 import cn from 'classnames';
 
 import { inter, sen } from '@shared/lib/fonts';
--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
+
+import cn from 'classnames';
 
 import styles from './index.module.scss';
 import { routes } from './lib/routes';
@@ -6,19 +10,39 @@ import { routes } from './lib/routes';
 import { Button } from '@shared/ui/Button';
 
 export const Header = () => {
+	const [isOpenedMenu, setIsOpenedMenu] = useState<boolean>(false);
+
+	const handleToggleOpenedMenu = () => {
+		setIsOpenedMenu(!isOpenedMenu);
+	};
+
 	return (
 		<header className={styles.header}>
-			<h1 className={styles.title}>Modsen Client Blog</h1>
-			<nav className={styles.nav}>
-				{routes.map(({ path, label }) => (
-					<Link className={styles.navlink} href={path} key={path}>
-						{label}
-					</Link>
-				))}
-			</nav>
-			<Button className={styles.video} type="secondary">
-				Video about us
-			</Button>
+			<div
+				onClick={handleToggleOpenedMenu}
+				className={cn(styles.burger, {
+					[styles.active]: isOpenedMenu,
+				})}
+			>
+				<span></span>
+			</div>
+			<div
+				className={cn(styles.headerWrapper, {
+					[styles.active]: isOpenedMenu,
+				})}
+			>
+				<h1 className={styles.title}>Modsen Client Blog</h1>
+				<nav className={styles.nav}>
+					{routes.map(({ path, label }) => (
+						<Link className={styles.navlink} href={path} key={path}>
+							{label}
+						</Link>
+					))}
+				</nav>
+				<Button className={styles.video} type="secondary">
+					Video about us
+				</Button>
+			</div>
 		</header>
 	);
 };
