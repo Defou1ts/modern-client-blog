10f4ab18de2514aa97c103dbea6177c137de9a55
Defou1ts <pasnkevikn@gmail.com>
2023-04-21 22:47:07 +0300

    fix: homepage trash

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,10 @@ import type { GetStaticProps } from 'next';
 
 import { MainContainer } from '@app/MainContainer';
 import { H } from '@shared/ui/H';
-import { P } from '@shared/ui/P';
 
 const Index = () => (
 	<MainContainer title="Home | Modsen client blog" description="Modsen client blog">
-		<div>Hello</div>
-		<H type="display">Hello</H>
-		<P type="medium">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda explicabo commodi iste
-			necessitatibus, debitis natus aspernatur id sit cum perspiciatis fugiat consectetur adipisci exercitationem
-			dolorum repellat quam aliquam alias expedita deleniti vel? Reprehenderit rerum quos iste, odit consequatur
-			similique.
-		</P>
-		<P type="small">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda explicabo commodi iste
-			necessitatibus, debitis natus aspernatur id sit cum perspiciatis fugiat consectetur adipisci exercitationem
-			dolorum repellat quam aliquam alias expedita deleniti vel? Reprehenderit rerum quos iste, odit consequatur
-			similique.
-		</P>
-		<P type="label">
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam assumenda explicabo commodi iste
-			necessitatibus, debitis natus aspernatur id sit cum perspiciatis fugiat consectetur adipisci exercitationem
-			dolorum repellat quam aliquam alias expedita deleniti vel? Reprehenderit rerum quos iste, odit consequatur
-			similique.
-		</P>
+		<H type="display">Home</H>
 	</MainContainer>
 );
 
