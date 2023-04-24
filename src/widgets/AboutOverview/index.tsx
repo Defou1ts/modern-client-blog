435e6cc98cd82c10dcbd7194e6e80db4a18e82ef
Defou1ts <pasnkevikn@gmail.com>
2023-04-24 09:59:24 +0300

    feat: about overview block

--- src/widgets/AboutOverview/index.tsx
+++ src/widgets/AboutOverview/index.tsx
@@ -1,10 +1,12 @@
 import styles from './index.module.scss';
 import { AboutOverviewContent } from './ui/AboutOverviewContent';
 import { AboutOverviewHeader } from './ui/AboutOverviewHeader';
+import { AboutOverviewOurTasks } from './ui/AboutOverviewOurTasks';
 
 export const AboutOverview = () => (
 	<div className={styles.wrapper}>
 		<AboutOverviewHeader />
 		<AboutOverviewContent />
+		<AboutOverviewOurTasks />
 	</div>
 );
