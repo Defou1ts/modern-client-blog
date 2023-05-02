29e33e3e6b23f1c820d40cd7d94e49f6c4818c31
Defou1ts <pasnkevikn@gmail.com>
2023-05-02 11:13:51 +0300

    feat: initialized homepage with existing widgets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,48 @@ import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import type { GetStaticProps } from 'next';
 
 import { MainContainer } from '@app/wrappers/MainContainer';
-import { H } from '@shared/ui/H';
 import { defaultLocale } from '@shared/contants/defaultLocale';
-import { CategoryTag } from '@shared/ui/CategoryTag';
+import { HomePageWrapper } from '@app/wrappers/HomePageWrapper';
+import { JoinOurTeam } from '@widgets/JoinOurTeam';
+import { AuthorList } from '@entities/Author/ui/AuthorList';
+import { authors } from '@entities/Author/lib/mock/authors';
+import { HomeCategoriesList } from '@widgets/HomeCategoriesList/BlogPostCategoriesList';
+import { allPostCategories } from '@entities/Post/lib/mock/allPostCategories';
 
-const Index = () => (
+import type { PostCategory } from '@entities/Post/interfaces';
+import type { AuthorWithLocales } from '@entities/Author/interfaces';
+
+interface HomePageProps {
+	authors: AuthorWithLocales[];
+	categories: PostCategory[];
+}
+
+const HomePage = ({ authors, categories }: HomePageProps) => (
 	<MainContainer title="Home | Modsen client blog" description="Modsen client blog">
-		<H type="display">Home</H>
-		<CategoryTag tagName="Life" isActive type="medium" />
-		<CategoryTag tagName="Experience" type="medium" />
-		<CategoryTag tagName="Business" type="medium" />
+		<HomePageWrapper>
+			<article>1</article>
+			<article>1</article>
+			<article>1</article>
+			<HomeCategoriesList categories={categories} />
+			<article>1</article>
+			<AuthorList authors={authors} />
+			<article>1</article>
+			<article>1</article>
+			<article>
+				<JoinOurTeam />
+			</article>
+		</HomePageWrapper>
 	</MainContainer>
 );
 
-export default Index;
+export default HomePage;
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
 	return {
 		props: {
 			...(await serverSideTranslations(locale ?? defaultLocale, ['common'])),
+			authors,
+			categories: allPostCategories,
 		},
 	};
 };
