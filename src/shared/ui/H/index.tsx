079bb551e42a050b12720fe0075596cfdc98a5e2
Defou1ts <pasnkevikn@gmail.com>
2023-05-15 20:17:00 +0300

    feat: fixed switch case on H component

--- src/shared/ui/H/index.tsx
+++ src/shared/ui/H/index.tsx
@@ -5,49 +5,11 @@ import styles from './index.module.scss';
 import type { HProps } from './interfaces';
 
 export const H = ({ type, children, className, ...props }: HProps) => {
-	switch (type) {
-		case 'display':
-		case 'h1':
-			return (
-				<h1 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h1>
-			);
-		case 'h2':
-			return (
-				<h2 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h2>
-			);
-		case 'h3':
-			return (
-				<h3 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h3>
-			);
-		case 'h4':
-			return (
-				<h4 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h4>
-			);
-		case 'h5':
-			return (
-				<h5 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h5>
-			);
-		case 'h6':
-			return (
-				<h6 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h6>
-			);
-		default:
-			return (
-				<h1 className={cn(styles.h, styles[type], className)} {...props}>
-					{children}
-				</h1>
-			);
-	}
+	const HTag = type === 'display' ? 'h1' : type;
+
+	return (
+		<HTag className={cn(styles.h, styles[type], className)} {...props}>
+			{children}
+		</HTag>
+	);
 };
