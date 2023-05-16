04b8746229773a64488991e73e12774b79eac7c2
Defou1ts <pasnkevikn@gmail.com>
2023-05-16 10:04:17 +0300

    fix: separated content data on Privacy widget

--- src/widgets/Privacy/index.tsx
+++ src/widgets/Privacy/index.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'next-i18next';
 
 import styles from './index.module.scss';
+import { content } from './lib/constants/content';
 
 import { useLocale } from '@shared/lib/hooks/useLocale';
 import { Heading } from '@shared/ui/Heading';
@@ -24,50 +25,18 @@ export const Privacy = () => {
 				</Paragraph>
 			</section>
 			<section className={styles.content}>
-				<div className={styles.contentBlock}>
-					<Heading type="h1" className={styles.subtitle}>
-						Lorem ipsum dolor sit amet
-					</Heading>
-					<Paragraph type="medium" className={styles.text}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-						voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet,
-						consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-						Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At risus viverra
-						adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient montes. Ridiculus mus
-						mauris vitae ultricies leo. Neque egestas congue quisque egestas diam. Risus in hendrerit
-						gravida rutrum quisque non. Sit amet nulla facilisi morbi tempus iaculis urna. Lorem sed risus
-						ultricies tristique nulla aliquet enim. Volutpat blandit aliquam etiam erat velit. Orci eu
-						lobortis elementum nibh. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla.
-						Neque convallis a cras semper auctor neque vitae tempus quam.
-					</Paragraph>
-				</div>
-				<div className={styles.contentBlock}>
-					<Heading type="h2" className={styles.subtitle}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.
-					</Heading>
-					<Paragraph type="medium" className={styles.text}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam
-						sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient
-						montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam.
-						Risus in hendrerit gravida rutrum quisque non. Sit amet nulla facilisi morbi tempus iaculis
-						urna. Lorem sed risus ultricies tristique nulla aliquet enim. Volutpat blandit aliquam etiam
-						erat velit. Orci eu lobortis elementum nibh. Ipsum suspendisse ultrices gravida dictum fusce ut
-						placerat orci nulla. Neque convallis a cras semper auctor neque vitae tempus quam.
-					</Paragraph>
-					<Paragraph type="medium" className={styles.text}>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam
-						sem. At risus viverra adipiscing at in tellus. Sociis natoque penatibus et magnis dis parturient
-						montes. Ridiculus mus mauris vitae ultricies leo. Neque egestas congue quisque egestas diam.
-						Risus in hendrerit gravida rutrum quisque non. Sit amet nulla facilisi morbi tempus iaculis
-						urna. Lorem sed risus ultricies tristique nulla aliquet enim. Volutpat blandit aliquam etiam
-						erat velit. Orci eu lobortis elementum nibh. Ipsum suspendisse ultrices gravida dictum fusce ut
-						placerat orci nulla. Neque convallis a cras semper auctor neque vitae tempus quam.
-					</Paragraph>
-				</div>
+				{content.map(({ title, paragraphs }) => (
+					<div className={styles.contentBlock} key={title}>
+						<Heading type="h1" className={styles.subtitle}>
+							{title}
+						</Heading>
+						{paragraphs.map((paragraph) => (
+							<Paragraph key={paragraph} type="medium" className={styles.text}>
+								{paragraph}
+							</Paragraph>
+						))}
+					</div>
+				))}
 			</section>
 		</div>
 	);
