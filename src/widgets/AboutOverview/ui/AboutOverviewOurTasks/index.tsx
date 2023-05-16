fdce74a17675af9c387719f20e5db831c326e129
Defou1ts <pasnkevikn@gmail.com>
2023-05-16 13:38:25 +0300

    fix: refactored AboutOverview

--- src/widgets/AboutOverview/ui/AboutOverviewOurTasks/index.tsx
+++ src/widgets/AboutOverview/ui/AboutOverviewOurTasks/index.tsx
@@ -4,34 +4,24 @@ import styles from './index.module.scss';
 
 import { Heading } from '@shared/ui/Heading';
 import { Paragraph } from '@shared/ui/Paragraph';
+import { ourTasks } from '@widgets/AboutOverview/lib/constants/ourTasks';
 
 export const AboutOverviewOurTasks = () => {
 	const { t } = useTranslation();
 
 	return (
 		<div className={styles.wrapper}>
-			<div className={styles.item}>
-				<p className={styles.subtitle}>{t('about.overview.our-mission-subtitle')}</p>
-				<Heading type="h3" className={styles.title}>
-					{t('about.overview.our-mission-title')}
-				</Heading>
-				<Paragraph type="medium" className={styles.text}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At
-					risus viverra adipiscing at in tellus.
-				</Paragraph>
-			</div>
-			<div className={styles.item}>
-				<p className={styles.subtitle}>{t('about.overview.our-vision-subtitle')}</p>
-				<Heading type="h3" className={styles.title}>
-					{t('about.overview.our-vision-title')}
-				</Heading>
-				<Paragraph type="medium" className={styles.text}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Non blandit massa enim nec. Scelerisque viverra mauris in aliquam sem. At
-					risus viverra adipiscing at in tellus.
-				</Paragraph>
-			</div>
+			{ourTasks.map(({ titleLanguagePath, subtitleLanguagePath, text }) => (
+				<div key={titleLanguagePath} className={styles.item}>
+					<p className={styles.subtitle}>{t(subtitleLanguagePath)}</p>
+					<Heading type="h3" className={styles.title}>
+						{t(titleLanguagePath)}
+					</Heading>
+					<Paragraph type="medium" className={styles.text}>
+						{text}
+					</Paragraph>
+				</div>
+			))}
 		</div>
 	);
 };
--- src/widgets/Footer/ui/Social/index.tsx
+++ src/widgets/Footer/ui/Social/index.tsx
@@ -8,14 +8,12 @@ import { SocialLink } from '@shared/ui/SocialLink';
 export const Social = () => {
 	return (
 		<div className={styles.social}>
-			<div>
-				<Paragraph className={styles.adressItem} type="medium">
-					Finstreet 118 2561 Fintown
-				</Paragraph>
-				<Paragraph className={styles.adressItem} type="medium">
-					Hello@finsweet.com 020 7993 2905
-				</Paragraph>
-			</div>
+			<Paragraph className={styles.adressItem} type="medium">
+				Finstreet 118 2561 Fintown
+			</Paragraph>
+			<Paragraph className={styles.adressItem} type="medium">
+				Hello@finsweet.com 020 7993 2905
+			</Paragraph>
 			<div className={styles.socialList}>
 				{socialIcons.map((props) => (
 					<SocialLink key={props.path} {...props} />
