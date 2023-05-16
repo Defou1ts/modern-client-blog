7379ff3dac5fd8ef35c53827a3dc9d2dd69fe186
Defou1ts <pasnkevikn@gmail.com>
2023-05-16 09:26:22 +0300

    fix: usePostList refactoring

--- src/entities/Post/lib/hooks/usePostList.ts
+++ src/entities/Post/lib/hooks/usePostList.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 
 import type { Post } from '../../interfaces';
 
@@ -6,18 +6,16 @@ export const usePostList = (posts: Post[], maxPosts: number) => {
 	const [currentPage, setCurrentPage] = useState(0);
 
 	const handlePrevClick = () => {
-		setCurrentPage(currentPage - 1);
+		setCurrentPage((prev) => prev - 1);
 	};
 
 	const handleNextClick = () => {
-		setCurrentPage(currentPage + 1);
+		setCurrentPage((prev) => prev + 1);
 	};
 
-	const isPrevDisabled = useMemo(() => currentPage === 0, [currentPage]);
-	const isNextDisabled = useMemo(
-		() => currentPage * maxPosts + maxPosts >= posts.length,
-		[currentPage, maxPosts, posts]
-	);
+	const isPrevDisabled = currentPage === 0;
+	const isNextDisabled = currentPage * maxPosts + maxPosts >= posts.length;
+
 	const needButtons = !(isNextDisabled && isPrevDisabled);
 
 	const postsToRender = posts.slice(currentPage * maxPosts, currentPage * maxPosts + maxPosts);
