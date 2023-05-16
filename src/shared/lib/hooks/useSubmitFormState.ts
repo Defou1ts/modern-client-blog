5bee26200d53bf0493dd2a08d8519103726e07e0
Defou1ts <pasnkevikn@gmail.com>
2023-05-16 12:59:49 +0300

    fix:setLoadingStatus function name

--- src/shared/lib/hooks/useSubmitFormState.ts
+++ src/shared/lib/hooks/useSubmitFormState.ts
@@ -3,10 +3,10 @@ import { useState } from 'react';
 export const useSubmitFormState = () => {
 	const [isSuccess, setIsSuccess] = useState(false);
 	const [isError, setIsError] = useState(false);
-	const [isLoading, setIsLodaing] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
 	const setSuccesWithTimeout = (time: number) => {
-		setIsLodaing(false);
+		setIsLoading(false);
 		setIsSuccess(true);
 		setTimeout(() => {
 			setIsSuccess(false);
@@ -14,7 +14,7 @@ export const useSubmitFormState = () => {
 	};
 
 	const setErrorWithTimeout = (time: number) => {
-		setIsLodaing(false);
+		setIsLoading(false);
 		setIsError(false);
 		setTimeout(() => {
 			setIsError(false);
@@ -22,7 +22,7 @@ export const useSubmitFormState = () => {
 	};
 
 	const setLoading = () => {
-		setIsLodaing(true);
+		setIsLoading(true);
 	};
 
 	return { isSuccess, isLoading, isError, setSuccesWithTimeout, setErrorWithTimeout, setLoading };
